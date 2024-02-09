41737b68bf9c738d97e70f06826fac4554bedfa9
William Jay Inclino <wjay.inclino@gmail.com>
2024-02-09 10:24:15 +0800

    implement update function in rr item module

--- apps/warehouse/src/rr-item/rr-item.resolver.ts
+++ apps/warehouse/src/rr-item/rr-item.resolver.ts
@@ -1,35 +1,52 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { RrItemService } from './rr-item.service';
 import { RrItem } from './entities/rr-item.entity';
 import { CreateRRItemInput } from './dto/create-rr-item.input';
 import { UpdateRrItemInput } from './dto/update-rr-item.input';
+import { WarehouseRemoveResponse } from '../__common__/classes';
 
 @Resolver(() => RrItem)
 export class RrItemResolver {
   constructor(private readonly rrItemService: RrItemService) {}
 
   @Mutation(() => RrItem)
-  createRrItem(@Args('createRrItemInput') createRrItemInput: CreateRRItemInput) {
+  createRrItem(@Args('input') createRrItemInput: CreateRRItemInput) {
     return this.rrItemService.create(createRrItemInput);
   }
 
-  @Query(() => [RrItem], { name: 'rrItem' })
-  findAll() {
+  @Query(() => [RrItem])
+  async rr_items(
+    @Args('rr_id', {nullable: true}) rr_id?: string,
+  ) {
+    if(rr_id){
+      return await this.rrItemService.findByRrId(rr_id)
+    }
     return this.rrItemService.findAll();
   }
 
-  @Query(() => RrItem, { name: 'rrItem' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.rrItemService.findOne(id);
+  @Query(() => RrItem)
+  async rr_item(
+    @Args('id', {nullable: true}) id?: string,
+    @Args('rr_number', {nullable: true}) rr_number?: string,
+  ) {
+    if(id){
+      return await this.rrItemService.findOne(id);
+    }
+    if(rr_number){
+      return await this.rrItemService.findByRrNumber(rr_number) 
+    }
   }
 
-  // @Mutation(() => RrItem)
-  // updateRrItem(@Args('updateRrItemInput') updateRrItemInput: UpdateRrItemInput) {
-  //   return this.rrItemService.update(updateRrItemInput.id, updateRrItemInput);
-  // }
-
   @Mutation(() => RrItem)
-  removeRrItem(@Args('id', { type: () => Int }) id: number) {
+  updateRrItem(
+    @Args('id') id: string,
+    @Args('input') updateRrItemInput: UpdateRrItemInput
+  ) {
+    return this.rrItemService.update(id, updateRrItemInput);
+  }
+
+  @Mutation(() => WarehouseRemoveResponse)
+  removeRrItem(@Args('id') id: string) {
     return this.rrItemService.remove(id);
   }
 }
--- apps/warehouse/src/rr-item/rr-item.service.ts
+++ apps/warehouse/src/rr-item/rr-item.service.ts
@@ -1,52 +1,286 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreateRRItemInput } from './dto/create-rr-item.input';
 import { UpdateRrItemInput } from './dto/update-rr-item.input';
 import { PrismaService } from '../__prisma__/prisma.service';
 import { Prisma, RRItem } from 'apps/warehouse/prisma/generated/client';
+import { ITEM_TRANSACTION_TYPE } from '../__common__/types';
+import { WarehouseRemoveResponse } from '../__common__/classes';
 
 @Injectable()
 export class RrItemService {
 
 	private readonly logger = new Logger(RrItemService.name)
+	private includedFields = {
+		rr: true
+	}
 
     constructor(
         private readonly prisma: PrismaService,
     ) {}
-
-	async create(input: CreateRRItemInput){
+	
+	// create rr item and item transaction
+	// update item quantity
+	async create(input: CreateRRItemInput): Promise<RRItem>{
 		
-		// this.logger.log('create')
+		this.logger.log('create')
 
-		// const data: Prisma.RRItemCreateInput = {
-		// 	rr: { connect: { id: input.rr_id } },
-		// 	item: input.item_id ? { connect: { id: input.item_id } } : undefined,
-		// 	item_brand: input.item_brand_id ? { connect: { id: input.item_brand_id } } : undefined,
-		// 	unit: input.unit_id ? { connect: { id: input.unit_id } } : undefined,
-		// 	item_class: input.item_class,
-		// 	quantity_delivered: input.quantity_delivered,
-		// 	quantity_accepted: input.quantity_accepted,
-		// 	description: input.description,
-		// 	vat_type: input.vat_type,
-		// 	gross_price: input.gross_price,
-		// 	net_price: input.net_price,
-		// 	freight_cost: input.freight_cost,
-		// }
+		const itemTransaction: Prisma.ItemTransactionCreateWithoutRr_itemInput = {
+			item: { connect: { id: input.item_id } },
+			type: ITEM_TRANSACTION_TYPE.STOCK_IN,
+			quantity: input.quantity_accepted,
+			price: input.net_price
+		}
+
+		const data: Prisma.RRItemCreateInput = {
+			rr: { connect: { id: input.rr_id } },
+			item: input.item_id ? { connect: { id: input.item_id } } : undefined,
+			item_brand: input.item_brand_id ? { connect: { id: input.item_brand_id } } : undefined,
+			unit: input.unit_id ? { connect: { id: input.unit_id } } : undefined,
+			item_class: input.item_class,
+			quantity_delivered: input.quantity_delivered,
+			quantity_accepted: input.quantity_accepted,
+			description: input.description,
+			vat_type: input.vat_type,
+			gross_price: input.gross_price,
+			net_price: input.net_price,
+			freight_cost: input.freight_cost,
+			item_transaction: {
+				create: itemTransaction
+			}
+		}
+
+		const createRRItemQuery = this.prisma.rRItem.create({
+            data,
+            include: this.includedFields
+        })
+		
+		const item = await this.prisma.item.findUnique({
+			where: {
+				id: input.item_id
+			}
+		})
+
+		if(!item) {
+            throw new NotFoundException('Item not found')
+        }
+
+		const totalQuantity = item.quantity + input.quantity_accepted
+
+		const updateItemQuery = this.prisma.item.update({
+			where: {
+				id: input.item_id
+			},
+			data: {
+				quantity: totalQuantity
+			}
+		})
+
+
+		const [createdRRItem, updatedItem] = await this.prisma.$transaction([
+			createRRItemQuery,
+			updateItemQuery
+		])
+
+        this.logger.log('Successfully created RR Item and updated Item quantity')
+
+        return createdRRItem
 
 	}
 
-	findAll() {
-		return `This action returns all rrItem`;
+	async findAll(): Promise<RRItem[]> {
+
+		return await this.prisma.rRItem.findMany({
+			where: { is_deleted: false },
+			include: this.includedFields
+		})
+
 	}
 
-	findOne(id: number) {
-		return `This action returns a #${id} rrItem`;
+	async findByRrId(rrId: string): Promise<RRItem[]> {
+
+		return await this.prisma.rRItem.findMany({
+			where: {
+				is_deleted: false,
+				rr_id: rrId
+			},
+			include: this.includedFields
+		})
+
 	}
 
-	update(id: number, updateRrItemInput: UpdateRrItemInput) {
-		return `This action updates a #${id} rrItem`;
+	async findByRrNumber(rrNumber: string): Promise<RRItem[]> {
+
+		return await this.prisma.rRItem.findMany({
+			where: {
+				is_deleted: false,
+				rr: {
+					rr_number: rrNumber
+				}
+			},
+			include: this.includedFields
+		})
+
 	}
 
-	remove(id: number) {
-		return `This action removes a #${id} rrItem`;
+	async findOne(id: string): Promise<RRItem | null> {
+        const item = await this.prisma.rRItem.findUnique({
+            include: this.includedFields,
+            where: { id }
+        })
+
+        if(!item) {
+            throw new NotFoundException('RR Item not found')
+        }
+
+        return item 
+	}
+
+	async update(id: string, input: UpdateRrItemInput): Promise<RRItem> {
+
+        this.logger.log('update(')
+
+        const existingItem = await this.findOne(id)
+
+		const data: Prisma.RRItemUpdateInput = {
+			item: input.item_id ? 
+				{
+					connect: { id: input.item_id }
+				}
+				:
+				{
+					connect: { id: existingItem.item_id }
+				},
+			item_brand: input.item_brand_id ? 
+				{
+					connect: { id: input.item_brand_id }
+				}
+				:
+				{
+					connect: { id: existingItem.item_brand_id }
+				},
+			unit: input.unit_id ? 
+				{
+					connect: { id: input.unit_id }
+				}
+				:
+				{
+					connect: { id: existingItem.unit_id }
+				},
+			item_class: input.item_class ?? existingItem.item_class,
+			quantity_delivered: input.quantity_delivered ?? existingItem.quantity_delivered,
+			quantity_accepted: input.quantity_accepted ?? existingItem.quantity_accepted,
+			description: input.description ?? existingItem.description,
+			vat_type: input.vat_type ?? existingItem.vat_type,
+			gross_price: input.gross_price ?? existingItem.gross_price,
+			net_price: input.net_price ?? existingItem.net_price,
+			freight_cost: input.freight_cost ?? existingItem.freight_cost,
+		}
+
+
+		// if no quantity_accepted or net price to update
+		if(!input.quantity_accepted || !input.net_price) {
+			const updated = await this.prisma.rRItem.update({
+				data,
+				where: { id },
+				include: this.includedFields
+			})
+
+			this.logger.log('Successfully updated RR Item')
+
+			return updated
+		}
+
+
+		// initialize update rr item query
+		const updateRRItemQuery = this.prisma.rRItem.update({
+			data,
+			where: { id },
+			include: this.includedFields
+		})
+
+
+		// initialize update item transaction query to update it's new quantity and price base on rr item
+        const itemTransaction = await this.prisma.itemTransaction.findUnique({
+            where: { rr_item_id: id }
+        })
+
+		if(!itemTransaction) {
+            throw new NotFoundException('Item transaction not found')
+        }
+
+		const dataItemTransaction: Prisma.ItemTransactionUpdateInput = {
+			quantity: input.quantity_accepted ?? itemTransaction.quantity,
+			price: input.net_price ?? itemTransaction.price
+		}
+
+		const updateItemTransactionQuery = this.prisma.itemTransaction.update({
+			data: dataItemTransaction,
+			where: {
+				id: itemTransaction.id
+			}
+		})
+
+		// if no quantity_accepted then update only rr item and item transaction. No need to update item new quantity
+		if(!input.quantity_accepted){
+
+			const [updatedRRItem, updatedItemTransaction] = await this.prisma.$transaction([
+				updateRRItemQuery,
+				updateItemTransactionQuery
+			])
+
+			this.logger.log('Successfully updated RR Item and Item transaction')
+
+			return updatedRRItem
+
+		}
+
+		// initialize update item query to update it's new quantity
+        const item = await this.prisma.item.findUnique({
+            where: { id: itemTransaction.item_id }
+        })
+
+        if(!item) {
+            throw new NotFoundException('Item not found')
+        }
+
+		const newQuantity = (item.quantity - itemTransaction.quantity) + input.quantity_accepted
+
+		const dataItem: Prisma.ItemUpdateInput = {
+			quantity: newQuantity
+		}
+
+		const updateItemQuery = this.prisma.item.update({
+			where: {
+				id: item.id
+			},
+			data: dataItem
+		})
+
+		// update rr item, item transaction's quantity and price, item's quantity using database transaction
+
+		const [updatedRRItem, updatedItemTransaction, updatedItem] = await this.prisma.$transaction([
+			updateRRItemQuery,
+			updateItemTransactionQuery,
+			updateItemQuery
+		])
+
+		this.logger.log('Successfully updated RR Item, Item transaction, and Item')
+
+		return updatedRRItem
+
+	}
+
+	async remove(id: string): Promise<WarehouseRemoveResponse> {
+		const existingItem = await this.findOne(id)
+
+		await this.prisma.rRItem.update( {
+			where: { id },
+			data: { is_deleted: true }
+		} )
+
+		return {
+			success: true,
+			msg: "RR Item successfully deleted"
+		}
 	}
 }
